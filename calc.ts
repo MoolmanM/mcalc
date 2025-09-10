350fe6cacc134f11491e95951b50cdc2e6cc9736
MoolmanM <mmoolman067@gmail.com>
2025-09-10 10:16:16 +0200

    feat: add new calc algorithm

--- calc.ts
+++ calc.ts
@@ -1,52 +1,12 @@
-function linearSearch(arr: string[], target: string): number[] {
-  const outputArr: number[] = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      outputArr.push(i)
-    }
-  }
-  return outputArr
-}
-
 export function calc(inputArr: string[]): number {
-  let total = 0;
-  const parenthesesIndex = linearSearch(inputArr, "(");
-  const exponenetsIndex = linearSearch(inputArr, "*");
-  const multiplicationIndex = linearSearch(inputArr, "*");
-  const divisionIndex = linearSearch(inputArr, "/");
-  const additionIndex = linearSearch(inputArr, "+");
-  const subtractionIndex = linearSearch(inputArr, "-");
-
-  if (parenthesesIndex.length > 0) {
-
-  }
-  if (exponenetsIndex.length > 0) {
-
-  }
-  if (multiplicationIndex.length > 0) {
-    for (let i = 0; i < multiplicationIndex.length; i++) {
-      const left = Number(inputArr[multiplicationIndex[i] - 1]);
-      const right = Number(inputArr[multiplicationIndex[i] + 1]);
-      total = left * right;
-      inputArr[multiplicationIndex[i] - 1] = total.toString();
-      inputArr.splice(multiplicationIndex[i], 2)
-    }
-  }
-  if (divisionIndex.length > 0) {
-
-  }
-  if (additionIndex.length > 0) {
-    for (let i = 0; i < additionIndex.length; i++) {
-      const left = Number(inputArr[additionIndex[i] - 1]);
-      const right = Number(inputArr[additionIndex[i] + 1]);
-      total = left + right;
-      inputArr[additionIndex[i] - 1] = total.toString();
-      inputArr.splice(additionIndex[i], 2)
-    }
-
-  }
-  if (subtractionIndex.length > 0) {
-
+  let i = 0;
+  while ((i = inputArr.indexOf("+")) !== -1) {
+    console.log(inputArr)
+    const left = Number(inputArr[i - 1]);
+    const right = Number(inputArr[i + 1]);
+    const total = left + right;
+    inputArr[i - 1] = total.toString();
+    inputArr.splice(i, 2);
   }
   return Number(inputArr[0])
 }
